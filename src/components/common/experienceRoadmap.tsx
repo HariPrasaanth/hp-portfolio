86444e210175d27039b32fd2211184f83a8a4178
Hari Babu <hari.babu@perficient.com>
2025-08-17 22:32:25 +0530

    Fix padding in ExperienceRoadMap component for improved layout

--- src/components/common/experienceRoadmap.tsx
+++ src/components/common/experienceRoadmap.tsx
@@ -5,7 +5,7 @@ const ExperienceRoadMap: React.FC = () => {
   return (
     <div className="bg-[#e3e3e3] rounded">
       <div>
-        <p className=" text-[#2D2D2D] font-medium text-lg sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-center my-8">
+        <p className="text-[#2D2D2D] font-medium text-lg sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-center pt-8 pb-8">
           Experience
         </p>
       </div>
